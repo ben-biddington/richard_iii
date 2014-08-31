17269ea6a5ed809a14f2c90b7aa295cc04727c01
Ben Biddington <ben.biddington@gmail.com>
2014-08-31 14:33:15 +1200

    [FAILING, PARTIAL] A basic example

--- test/the_basics.rb
+++ test/the_basics.rb
@@ -2,14 +2,50 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 module Richard
+  module Internal
+    class RequestLineParser
+      
+    end
+
+    class RequestLine
+      attr_reader :method, :uri
+      
+      def initialize(opts = {})
+        @method,@uri = opts[:method],opts[:uri]
+      end
+
+      def eql?(other)
+        return self.method == other.method && self.uri == other.uri
+      end
+    end
+  end
+end
+
+module Richard
   class III
     def initialize(opts={})
       @internet = opts[:internet] || fail("You need to supply :internet")
     end
 
     def exec(text)
-      "xxx"
+      lines = text.lines.map(&:chomp)
+
+      verb = lines.first.match(/^(\w+) /)[1]
+
+      @internet.execute Request.new(:verb => verb, :uri => 'https://api.twitter.com/1.1/statuses') # Request.new(:verb => 'GET', :url => 'xxx')
     end
+  end
+end
+
+class Request
+  attr_reader :verb, :uri
+
+  def initialize(opts={})
+    @verb,@uri = opts[:verb],opts[:uri]
+  end
+
+  def eql?(other)
+    self.verb.eql?(other.verb) && self.uri.eql?(other.uri)
   end
 end
 
@@ -17,16 +53,20 @@ describe 'The basics of Richard III' do
   it "can issue a very simple request" do
     spy_internet = MiniTest::Mock.new
 
-    spy_internet.expect :execute, nil, []
+    spy_internet.expect :execute, nil do |actual|
+      actual.first.eql? Request.new(:verb => 'GET', :uri => 'https://api.twitter.com/1.1/statuses')
+    end
 
     richard_iii = Richard::III.new :internet => spy_internet
 
     richard_iii.exec <<-TEXT 
-      GET /1.1/statuses
-      Host: api.twitter.com
-      Accept: application/json
+GET /1.1/statuses
+Host: api.twitter.com
+Accept: application/json
     TEXT
 
     spy_internet.verify
   end
+
+  # TEST: where does it read the protocol part (HTTP of HTTPS)
 end
