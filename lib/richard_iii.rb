5600da6c3c3766f2c4dfe20139aac89754ffabb2
Ben Biddington <ben.biddington@gmail.com>
2014-08-31 16:12:18 +1200

    Classes => lib/

--- lib/richard_iii.rb
+++ lib/richard_iii.rb
@@ -1,5 +1,31 @@
 require "richard_iii/version"
 
-module RichardIii
-  # Your code goes here...
+module Richard
+  class III
+    def initialize(opts={})
+      @internet = opts[:internet] || fail("You need to supply :internet")
+    end
+
+    def exec(text)
+      lines = text.lines.map(&:chomp).map(&:strip)
+
+      verb = lines.first.match(/^(\w+)/)[1]
+      path = lines.first.match(/(\S+)$/)[1]
+      host = lines[1].match(/Host: (.+)$/)[1]
+
+      @internet.execute Request.new(:verb => verb, :uri => "https://#{host}#{path}")
+    end
+  end
+
+  class Request
+    attr_reader :verb, :uri
+
+    def initialize(opts={})
+      @verb,@uri = opts[:verb],opts[:uri]
+    end
+
+    def eql?(other)
+      self.verb.eql?(other.verb) && self.uri.eql?(other.uri)
+    end
+  end
 end
--- test/the_basics.rb
+++ test/the_basics.rb
@@ -1,5 +1,4 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require File.join '.', 'test', 'helper'
 
 module Richard
   module Internal
@@ -14,36 +13,6 @@ module Richard
         return self.method == other.method && self.uri == other.uri
       end
     end
-  end
-end
-
-module Richard
-  class III
-    def initialize(opts={})
-      @internet = opts[:internet] || fail("You need to supply :internet")
-    end
-
-    def exec(text)
-      lines = text.lines.map(&:chomp).map(&:strip)
-
-      verb = lines.first.match(/^(\w+)/)[1]
-      path = lines.first.match(/(\S+)$/)[1]
-      host = lines[1].match(/Host: (.+)$/)[1]
-
-      @internet.execute Request.new(:verb => verb, :uri => "https://#{host}#{path}")
-    end
-  end
-end
-
-class Request
-  attr_reader :verb, :uri
-
-  def initialize(opts={})
-    @verb,@uri = opts[:verb],opts[:uri]
-  end
-
-  def eql?(other)
-    self.verb.eql?(other.verb) && self.uri.eql?(other.uri)
   end
 end
 
