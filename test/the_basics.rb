3a59118fcb21989597151492b372f083e3bc35b2
Ben Biddington <ben.biddington@gmail.com>
2014-08-31 13:50:58 +1200

    [FAILING, AMBER] A basic example

--- test/the_basics.rb
+++ test/the_basics.rb
@@ -1,13 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
+module Richard
+  class III
+    def initialize(opts={})
+      @internet = opts[:internet] || fail("You need to supply :internet")
+    end
+
+    def exec(text)
+      "xxx"
+    end
+  end
+end
+
 describe 'The basics of Richard III' do
   it "can issue a very simple request" do
     spy_internet = MiniTest::Mock.new
 
     spy_internet.expect :execute, nil, []
 
-    richard_iii = Richard::III.new spy_internet
+    richard_iii = Richard::III.new :internet => spy_internet
 
     richard_iii.exec <<-TEXT 
       GET /1.1/statuses
