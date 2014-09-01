6f3908ba2a689495a9d3819f261d01ef5fd149aa
Ben Biddington <ben.biddington@gmail.com>
2014-09-01 13:55:15 +1200

    [START HERE] A failing end-to-end test

--- Rakefile
+++ Rakefile
@@ -2,9 +2,16 @@ require "bundler/gem_tasks"
 
 require 'rake/testtask'
 
-Rake::TestTask.new do |t|
-  t.name = 'unit_test'		   
-  t.test_files = FileList['test/**/*.rb']
+namespace :test do 
+  Rake::TestTask.new do |t|
+    t.name = 'unit'		   
+    t.test_files = FileList['test/unit.tests/**/*.rb']
+  end
+
+  Rake::TestTask.new do |t|
+    t.name = 'acceptance'		   
+    t.test_files = FileList['test/acceptance.tests/**/*.rb']
+  end
 end
 
-task :default => :unit_test
+task :default => "test:unit"
---
+++ test/acceptance.tests/an_example.rb
@@ -0,0 +1,34 @@
+require File.join '.', 'test', 'helper'
+
+describe "An end-to-end example" do
+  it "lets me see raw text" do
+    when_I_request <<-TEXT 
+      GET /1.1/statuses
+      Host: api.twitter.com
+      Accept: application/json
+    TEXT
+
+   then_I_get <<-REPLY 
+      HTTP/1.1 400 Bad Request
+      content-length: 24
+      content-type: text/plain;charset=utf-8
+      date: Sat, 30 Aug 2014 01:30:43 UTC
+      server: tsa_a
+      set-cookie: guest_id=v1%3A140936224322485447; Domain=.twitter.com; Path=/; Expires=Mon, 29-Aug-2016 01:30:43 UTC
+      strict-transport-security: max-age=631138519
+      x-connection-hash: 7710cb2762e0a47702d21bb7e10d3056
+
+      Bad Authentication data
+    REPLY
+  end
+
+  private
+
+  def when_I_request(text)
+    fail "Not implemented"
+  end
+
+  def then_I_get(what)
+    fail "Not implemented"
+  end
+end
