92d47d0f4fa0a4d2d3688e46dac47f1504488f46
Ben Biddington <ben.biddington@gmail.com>
2014-09-03 08:00:59 +1200

    [WIP, FAILING] Request earls may be absolute or relative, [it] must have host header when relative

---
+++ test/unit.tests/earls_may_be_relative_or_absolute.rb
@@ -0,0 +1,15 @@
+require File.join '.', 'test', 'helper'
+
+describe "Request earls may be absolute or relative" do 
+  it "must have host header when relative" do
+    spy_internet = SpyInternet.new
+
+    richard_iii = Richard::III.new :internet => spy_internet
+
+    err = lambda { richard_iii.exec 'GET /1.1/statuses' }.must_raise RuntimeError
+
+    err.message.must_match /missing host header/
+  end
+
+  it "ignores host header when earl is absolute -- that is it does not send it"
+end
