2b39a6ecc97799f742473793d69ca6fffe4cd8e2
Ben Biddington <ben.biddington@gmail.com>
2014-09-02 20:34:33 +1200

    Split tests

--- test/unit.tests/can_partial_match_on_replies.rb
+++ test/unit.tests/can_partial_match_on_replies.rb
@@ -1,6 +1,6 @@
 require File.join '.', 'test', 'helper'
 
-describe "Full conversations" do 
+describe "Partial matching replies" do 
   before do
     @internet = SpyInternet.new
 
@@ -29,18 +29,47 @@ describe "Full conversations" do
     TEXT
   end
 
-  it "can be used to conduct a conversation" do
-    assert_equal @reply, <<-REPLY 
+  it "can match partially, so that you can ignore headers like date" do
+    expected = <<-REPLY 
       HTTP/1.1 400 Bad Request
-      content-length: 24
       content-type: text/plain;charset=utf-8
-      date: Sat, 30 Aug 2014 01:30:43 UTC
-      server: tsa_a
-      set-cookie: guest_id=v1%3A140936224322485447; Domain=.twitter.com; Path=/; Expires=Mon, 29-Aug-2016 01:30:43 UTC
-      strict-transport-security: max-age=631138519
-      x-connection-hash: 7710cb2762e0a47702d21bb7e10d3056
 
       Bad Authentication data
     REPLY
+
+    @reply.must_match expected
+  end
+
+  it "fails when the response line does not match" do
+    expected = <<-REPLY 
+      HTTP/1.1 XXX_BAD_RESPONSE_LINE
+      content-type: text/plain;charset=utf-8
+
+      Bad Authentication data
+    REPLY
+
+    @reply.must_not_match expected
+  end
+
+  it "fails, for example, when you expected a header that is not present" do
+    expected = <<-REPLY 
+      HTTP/1.1 400 Bad Request
+      xxx: this_one_is_not_present
+
+      Bad Authentication data
+    REPLY
+
+    @reply.must_not_match expected
+  end
+
+  it "fails when the body does not match" do
+    expected = <<-REPLY 
+      HTTP/1.1 400 Bad Request
+      content-type: text/plain;charset=utf-8
+
+      Who says famine has to be depressing
+    REPLY
+
+    @reply.must_not_match expected
   end
 end
