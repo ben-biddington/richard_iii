d9c2ab69ec8484b68475568d33892c0862eb3389
Ben Biddington <ben.biddington@gmail.com>
2014-09-16 12:22:03 +1200

    Pretty print xml with nokogiri like this

--- test/unit.tests/can_match_pretty_formatted_xml_bodies.rb
+++ test/unit.tests/can_match_pretty_formatted_xml_bodies.rb
@@ -22,4 +22,18 @@ describe "Matching pretty-formatted bodies" do
   it "matches when you say yes to pretty bodies" do
     @curl_reply.must_match "HTTP/1.1 200 OK"
   end
+
+  it "pretty print xml like this" do
+    require 'nokogiri'
+    
+    xml = <<-TEXT
+      <?xml version="1.0" encoding="utf-8"?><current><city id="6058560" name="London" /><humidity value="76" unit="%"/><pressure value="1018" unit="hPa"/></current>
+    TEXT
+    
+    doc = Nokogiri.XML(xml) do |config|
+      config.default_xml.noblanks
+    end
+
+    puts doc.to_xml(:indent => 2).lines.drop(1)
+  end
 end
