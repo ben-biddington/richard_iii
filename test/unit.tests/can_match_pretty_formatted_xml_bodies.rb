ebf383958fe310d6ae99945c4eef35f781706921
Ben Biddington <ben.biddington@gmail.com>
2014-09-16 12:15:23 +1200

    [WIP, FAILING] Can pretty match bodies

---
+++ test/unit.tests/can_match_pretty_formatted_xml_bodies.rb
@@ -0,0 +1,25 @@
+require File.join '.', 'test', 'helper'
+
+describe "Matching pretty-formatted bodies" do
+  before do
+    @curl_reply = CurlReply.new <<-TEXT
+      HTTP/1.1 200 OK
+      Content-Type: text/xml; charset=utf-8
+
+      <?xml version="1.0" encoding="utf-8"?><current><city id="6058560" name="London" /><humidity value="76" unit="%"/><pressure value="1018" unit="hPa"/></current>
+    TEXT
+  end
+
+  it "matches when you have neither pretty formatted" do
+    @curl_reply.must_match <<-TEXT
+      HTTP/1.1 200 OK
+      Content-Type: text/xml; charset=utf-8
+
+      <?xml version="1.0" encoding="utf-8"?><current><city id="6058560" name="London" /><humidity value="76" unit="%"/><pressure value="1018" unit="hPa"/></current>
+    TEXT
+  end
+
+  it "matches when you say yes to pretty bodies" do
+    @curl_reply.must_match "HTTP/1.1 200 OK"
+  end
+end
