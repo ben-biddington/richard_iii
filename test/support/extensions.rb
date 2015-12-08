9804e061966f7da030abcea3fcc23a77752da5fb
Ben Biddington <ben.biddington@gmail.com>
2015-12-09 11:42:13 +1300

    Update custom assertions for mintest 5

--- test/support/extensions.rb
+++ test/support/extensions.rb
@@ -1,7 +1,13 @@
-require 'minitest/unit'
-
 Richard::CurlReply.class_eval do
   include MiniTest::Assertions
+
+  def assertions=(what)
+    @assertions ||= what
+  end
+
+  def assertions
+    @assertions ||= 0
+  end
   
   def must_match(expected)
     assert(self.matches?(expected), "Expected <#{self}> to match <#{expected}>")
